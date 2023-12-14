34c9df1159b1c7a53e6cfd6d48591d4341a1dee1
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-12-14 17:56:00 +0700

    Question 1

---
+++ RecursiveStudent19.java
@@ -0,0 +1,23 @@
+public class RecursiveStudent19 {
+    static int factorialRecursive(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorialRecursive(n - 1);
+        }
+    }
+
+    static int factorialliterative(int n) {
+        int factorial = 1;
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        System.out.println(factorialRecursive(5));
+        System.out.println(factorialliterative(5));
+    }
+}
